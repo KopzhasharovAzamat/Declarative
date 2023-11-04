2de764cce4e0fe560af81fb8e4394ccc22e8e731
Azamat Kopzhasharov <kopzha01@gmail.com>
2023-11-04 11:19:03 +0600

    Добавьте файлы проекта.

---
+++ BLL/Configurations/AppConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BLL.Services;
+using BLL.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Configurations
+{
+    public static class AppConfiguration
+    {
+        public static void AddServices(this IServiceCollection service)
+        {
+            service.AddTransient<IHomeService, HomeService>();
+        }
+    }
+}
---
+++ BLL/Services/HomeService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class HomeService : IHomeService
+    {
+        public HomeService() 
+        {
+
+        }
+
+        public void Get(string id)
+        {
+            return;
+        }
+
+    }
+}
